26ff94129c6fc7b092832334db2eb2ba85177ed7
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-07 11:37:11 -0300

    feat: Add initial configs

---
+++ src/main/java/org/equipe_9/uniufc/TrabalhoBd20251UniUfcApplication.java
@@ -0,0 +1,13 @@
+package org.equipe_9.uniufc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoBd20251UniUfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabalhoBd20251UniUfcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/equipe_9/uniufc/TrabalhoBd20251UniUfcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.equipe_9.uniufc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoBd20251UniUfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
