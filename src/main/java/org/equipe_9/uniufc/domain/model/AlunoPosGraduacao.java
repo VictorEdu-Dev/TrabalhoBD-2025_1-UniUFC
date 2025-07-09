f695e74dd8f8b535e8f6bc343c8eecf1c55c9961
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 15:03:35 -0300

    feat: Add professorOrientador relationship to AlunoPosGraduacao entity

--- src/main/java/org/equipe_9/uniufc/domain/model/AlunoPosGraduacao.java
+++ src/main/java/org/equipe_9/uniufc/domain/model/AlunoPosGraduacao.java
@@ -1,19 +1,27 @@
 package org.equipe_9.uniufc.domain.model;
 
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @ToString
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "aluno_pos_graduacao")
 @Table(name = "alunos_pos_graduacao")
 @PrimaryKeyJoinColumn(name = "aluno_id")
 @DiscriminatorValue("POS_GRADUACAO")
 public final class AlunoPosGraduacao extends Aluno {
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "professor_orientador_id", nullable = false)
+    private Professor professorOrientador;
 }
