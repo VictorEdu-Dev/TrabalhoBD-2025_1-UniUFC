be75d304e2ae073add7648a1a017f2b5aa5639c2
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 00:02:21 -0300

    feat: Add Curso entity with attributes and relationships

---
+++ src/main/java/org/equipe_9/uniufc/domain/model/Curso.java
@@ -0,0 +1,35 @@
+package org.equipe_9.uniufc.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.equipe_9.uniufc.domain.CommonData;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "curso")
+public class Curso extends CommonData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10, unique = true)
+    private String codigo;
+
+    @Column(length = 200)
+    private String nome;
+
+    private Integer creditos;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Fetch(FetchMode.JOIN)
+    @JoinColumn(name = "departamento_id", nullable = false)
+    private Departamento departamento;
+
+}
