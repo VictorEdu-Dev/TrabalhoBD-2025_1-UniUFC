9ece935ca31d85e112317ad46df0465b568103c3
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 15:03:50 -0300

    feat: Add PGFormacaoBasica entity with relationships to AlunoPosGraduacao and Curso

---
+++ src/main/java/org/equipe_9/uniufc/domain/model/PGFormacaoBasica.java
@@ -0,0 +1,44 @@
+package org.equipe_9.uniufc.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.equipe_9.uniufc.domain.CommonData;
+
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "pg_formacao_basica")
+public class PGFormacaoBasica extends CommonData {
+    @Transient
+    private Long id;
+
+    @EmbeddedId
+    private PGFormacaoBasicaId idFormacaoBasica;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("alunoPosGraduacaoId")
+    @JoinColumn(name = "aluno_pos_graduacao_id", nullable = false)
+    private AlunoPosGraduacao alunoPosGraduacao;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("cursoId")
+    @JoinColumn(name = "curso_id", nullable = false)
+    private Curso curso;
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode
+    @Embeddable
+    public static class PGFormacaoBasicaId {
+        @Column(name = "aluno_pos_graduacao_id")
+        private Long alunoPosGraduacaoId;
+
+        @Column(name = "curso_id")
+        private Long cursoId;
+    }
+}
