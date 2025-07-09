bcb95f3031408c4037ff17e443c7abc623ef1adf
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 00:02:09 -0300

    feat: Add AlunoGraduacao entity with dataIngresso attribute

---
+++ src/main/java/org/equipe_9/uniufc/domain/model/AlunoGraduacao.java
@@ -0,0 +1,22 @@
+package org.equipe_9.uniufc.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "aluno_graduacao")
+@Table(name = "alunos_graduacao")
+@PrimaryKeyJoinColumn(name = "aluno_id")
+@DiscriminatorValue("GRADUACAO")
+public final class AlunoGraduacao extends Aluno {
+    @Column(columnDefinition = "datetime")
+    private LocalDateTime dataIngresso;
+}
+
