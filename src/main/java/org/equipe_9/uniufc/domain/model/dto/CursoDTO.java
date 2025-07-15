1ad6c5e3139a4fa4c65f364b117051ad4a55f905
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-15 17:34:02 -0300

    feat: Add CursoDTO for representing course data in the application

---
+++ src/main/java/org/equipe_9/uniufc/domain/model/dto/CursoDTO.java
@@ -0,0 +1,10 @@
+package org.equipe_9.uniufc.domain.model.dto;
+
+public record CursoDTO(
+        Long id,
+        String codigo,
+        String nome,
+        Integer creditos,
+        DepartamentoDTO departamento
+) {
+}
