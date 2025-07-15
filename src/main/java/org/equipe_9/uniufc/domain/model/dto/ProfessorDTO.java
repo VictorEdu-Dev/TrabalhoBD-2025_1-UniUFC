9468d6f3426ac4dcbbcf840b708c7feae09be876
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-15 17:34:41 -0300

    feat: Add ProfessorDTO for representing professor data in the application

---
+++ src/main/java/org/equipe_9/uniufc/domain/model/dto/ProfessorDTO.java
@@ -0,0 +1,17 @@
+package org.equipe_9.uniufc.domain.model.dto;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record ProfessorDTO(
+        Long id,
+        String siape,
+        String nomeCompleto,
+        LocalDateTime dataNascimento,
+        LocalDateTime dataIngresso,
+        Set<String> emails,
+        Set<String> telefones,
+        DepartamentoDTO departamento,
+        UsuarioDTO usuario
+) {
+}
