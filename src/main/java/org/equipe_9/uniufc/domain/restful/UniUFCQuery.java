93b2d202ed2160d4b97f588a788ccd3f657a65ff
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-15 17:35:02 -0300

    feat: Add UniUFCQuery for handling student and course queries in the API

---
+++ src/main/java/org/equipe_9/uniufc/domain/restful/UniUFCQuery.java
@@ -0,0 +1,31 @@
+package org.equipe_9.uniufc.domain.restful;
+
+import org.equipe_9.uniufc.domain.service.AlunoQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/uniufc")
+public class UniUFCQuery {
+    private AlunoQueryService alunoQueryService;
+
+
+    @GetMapping("/alunos-graduacao")
+    public ResponseEntity<?> getAlunosGraduacao() {
+        return ResponseEntity.ok(alunoQueryService.getAlunoGraduacao());
+    }
+
+    @GetMapping("/disciplinas")
+    public ResponseEntity<?> getDisciplinas() {
+        return ResponseEntity.ok(alunoQueryService.getDisciplinas());
+    }
+
+    @Autowired @Lazy
+    private void setAlunoQueryService(AlunoQueryService alunoQueryService) {
+        this.alunoQueryService = alunoQueryService;
+    }
+}
