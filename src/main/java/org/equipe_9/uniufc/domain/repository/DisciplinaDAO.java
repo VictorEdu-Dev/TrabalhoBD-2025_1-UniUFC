2dcaccbb535f6039b4d626571787acb2b65eb41e
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-15 17:34:19 -0300

    feat: Add DisciplinaDAO for CRUD operations on Disciplina entities

---
+++ src/main/java/org/equipe_9/uniufc/domain/repository/DisciplinaDAO.java
@@ -0,0 +1,7 @@
+package org.equipe_9.uniufc.domain.repository;
+
+import org.equipe_9.uniufc.domain.model.Disciplina;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DisciplinaDAO extends CrudRepository<Disciplina, Long> {
+}
