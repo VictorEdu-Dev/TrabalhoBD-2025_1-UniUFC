3d8978b32157408113424e0777decf4d19b366f4
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-16 11:40:48 -0300

    feat: Update API path for UniUFCQuery controller

--- src/main/java/org/equipe_9/uniufc/domain/restful/UniUFCQuery.java
+++ src/main/java/org/equipe_9/uniufc/domain/restful/UniUFCQuery.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/uniufc")
+@RequestMapping("/uniufc")
 public class UniUFCQuery {
     private AlunoQueryService alunoQueryService;
 
