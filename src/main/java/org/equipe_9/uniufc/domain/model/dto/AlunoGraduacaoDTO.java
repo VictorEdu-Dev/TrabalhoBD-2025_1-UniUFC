6e7c9908cce6cd4f195a2e9f9ad17e9091d504c3
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-16 11:40:18 -0300

    feat: Enhance AlunoGraduacaoDTO with additional nested DTOs for detailed student information

--- src/main/java/org/equipe_9/uniufc/domain/model/dto/AlunoGraduacaoDTO.java
+++ src/main/java/org/equipe_9/uniufc/domain/model/dto/AlunoGraduacaoDTO.java
@@ -1,9 +1,12 @@
 package org.equipe_9.uniufc.domain.model.dto;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
+@Builder(toBuilder = true)
 public record AlunoGraduacaoDTO(
         Long id,
         String matricula,
@@ -15,5 +18,36 @@ public record AlunoGraduacaoDTO(
         Set<TelefoneAlunoDTO> telefones,
         LocalDateTime dataIngresso
 ) {
+    @Builder(toBuilder = true)
     public record TelefoneAlunoDTO(String numero, String descricao) {}
+
+    @Builder(toBuilder = true)
+    public record AGDisciplinasDTO(
+            String nomeDisciplina,
+            String codigoDisciplina,
+            Integer creditosDisciplina
+    ) {}
+
+    @Builder(toBuilder = true)
+    public record CursoAlunoDTO(
+            String nome,
+            String matricula,
+            String curso,
+            String codigoCurso
+    ) {}
+
+    @Builder(toBuilder = true)
+    public record AlunoInfoNonDistinctDTO(
+            String nome,
+            String endereco,
+            String telefone,
+            String descricao
+    ) {}
+
+    @Builder(toBuilder = true)
+    public record AlunoInfoDTO(
+            String nome,
+            String endereco,
+            Set<TelefoneAlunoDTO> telefones
+    ) {}
 }
