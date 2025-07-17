f4ef5ccce848b9599767debfc11de30103cf6f85
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-16 22:57:52 -0300

    feat: Add nested records for CursoDepDTO and DepartamentoInfoDTO in DepartamentoDTO

--- src/main/java/org/equipe_9/uniufc/domain/model/dto/DepartamentoDTO.java
+++ src/main/java/org/equipe_9/uniufc/domain/model/dto/DepartamentoDTO.java
@@ -1,8 +1,32 @@
 package org.equipe_9.uniufc.domain.model.dto;
 
+import lombok.Builder;
+
+import java.util.List;
+@Builder(toBuilder = true)
 public record DepartamentoDTO(
         Long id,
         String nome,
         String codigo
 ) {
+    @Builder(toBuilder = true)
+    public record CursoDepDTO(
+            String codigoCurso,
+            String nome,
+            Integer creditos
+    ) {}
+    @Builder(toBuilder = true)
+    public record DepartamentoInfoDTO(
+            DepartamentoInfoNonDistinctDTO infoDepartamento,
+            List<CursoDTO.CursoInfoDTO> cursosOferecidos,
+            List<ProfessorDTO.ProfessoresInfoDTO> professores
+    ) {
+        @Builder(toBuilder = true)
+        public record DepartamentoInfoNonDistinctDTO(
+                String nome,
+                String codigo,
+                Long quantidadeCursos,
+                Long quantidadeprofessores
+        ) {}
+    }
 }
