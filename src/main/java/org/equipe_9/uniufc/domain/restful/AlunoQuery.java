2a6cdbe4724889c40788904bf163a94263e0cf2e
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-16 23:45:44 -0300

    feat: Remove unused imports from AlunoQuery.java

--- src/main/java/org/equipe_9/uniufc/domain/restful/AlunoQuery.java
+++ src/main/java/org/equipe_9/uniufc/domain/restful/AlunoQuery.java
@@ -19,9 +19,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Tag(name = "Aluno", description = "Consultas relacionadas ao aluno")
 @Validated
 @RestController
