7521fd4c44c2779742b2273280171494752867b0
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-18 11:58:30 -0300

    feat: Enhance CursoQuery with endpoints for optional and mandatory disciplines and students

--- src/main/java/org/equipe_9/uniufc/domain/restful/CursoQuery.java
+++ src/main/java/org/equipe_9/uniufc/domain/restful/CursoQuery.java
@@ -15,14 +15,48 @@ public class CursoQuery {
     private CursoQueryService cursoQueryService;
 
     @GetMapping(value = "/disciplinas-obrigatorias", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getDisciplinasObrigatorias(@RequestParam("curso") String nomeCurso) {
-        return ResponseEntity.ok(cursoQueryService.getDisciplinasObrigatorias(nomeCurso));
+    public ResponseEntity<?> getDisciplinasObrigatorias(@RequestParam("curso") Curso nomeCurso) {
+        return ResponseEntity.ok(cursoQueryService.getDisciplinasByTipo(nomeCurso.getNome(), "OBRIGATORIA"));
     }
 
+    @GetMapping(value = "/disciplinas-optativas", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getDisciplinasOptativas(@RequestParam("curso") Curso nomeCurso) {
+        return ResponseEntity.ok(cursoQueryService.getDisciplinasByTipo(nomeCurso.getNome(), "OPTATIVA"));
+    }
 
+    @GetMapping(value = "/alunos-curso", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getAlunosCurso(@RequestParam("curso") Curso nomeCurso) {
+        return ResponseEntity.ok(cursoQueryService.findAlunosCurso(nomeCurso.getNome()));
+    }
+
+    @GetMapping(value = "/alunos-curso-obrigatorias", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getAlunosCursoOp(@RequestParam("curso") Curso nomeCurso) {
+        return ResponseEntity.ok(cursoQueryService.findAlunosObrigatorias(nomeCurso.getNome()));
+    }
+
+    @GetMapping(value = "/alunos-curso-optativas", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getAlunosCursoOb(@RequestParam("curso") Curso nomeCurso) {
+        return ResponseEntity.ok(cursoQueryService.findAlunosOptativas(nomeCurso.getNome()));
+    }
 
     @Autowired
     private void  setCursoQueryService(CursoQueryService cursoQueryService) {
         this.cursoQueryService = cursoQueryService;
+    }
+
+    public enum Curso {
+        ENG_COMPT("Engenharia de Computação"),
+        ENG_ELET("Engenharia Elétrica"),
+        ODONTO("Odontologia");
+
+        private final String curso;
+
+        Curso(String curso) {
+            this.curso = curso;
+        }
+
+        public String getNome() {
+            return curso;
+        }
     }
 }
