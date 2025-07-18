7a3a653c59b22f561bcd6e4f43f6a2814378e7e0
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-18 11:58:34 -0300

    feat: Update CursoQueryService to include methods for fetching students and disciplines by type

--- src/main/java/org/equipe_9/uniufc/domain/service/CursoQueryService.java
+++ src/main/java/org/equipe_9/uniufc/domain/service/CursoQueryService.java
@@ -1,6 +1,6 @@
 package org.equipe_9.uniufc.domain.service;
 
-import org.equipe_9.uniufc.domain.model.dto.CursoDTO;
+import org.equipe_9.uniufc.domain.model.dto.AlunoGraduacaoDTO.AlunoSimpleDTO;
 import org.equipe_9.uniufc.domain.model.dto.DisciplinaDTO;
 import org.equipe_9.uniufc.domain.repository.CursoDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +12,24 @@ import java.util.List;
 public class CursoQueryService {
     private CursoDAO cursoDAO;
 
-    public List<DisciplinaDTO.DisciplinasInfoDTO> getDisciplinasObrigatorias(String nomeCurso) {
-        return cursoDAO.findDisciplinasObrigatorias(nomeCurso);
+    public List<DisciplinaDTO.DisciplinasInfoDTO> getDisciplinasByTipo(String nomeCurso, String tipo) {
+        return cursoDAO.findDisciplinasObrigatorias(nomeCurso, tipo);
+    }
+
+    public List<AlunoSimpleDTO> findAlunosCurso(String nomeCurso) {
+        return cursoDAO.findAlunosCurso(nomeCurso);
+    }
+
+    public List<AlunoSimpleDTO> findAlunosObrigatorias(String nomeCurso) {
+        return cursoDAO.findAlunosObrigatorias(nomeCurso);
+    }
+
+    public List<AlunoSimpleDTO> findAlunosOptativas(String nomeCurso) {
+        return cursoDAO.findAlunosCursoOp(nomeCurso);
     }
 
     @Autowired
     private void setCursoDAO(CursoDAO cursoDAO) {
         this.cursoDAO = cursoDAO;
     }
-
 }
