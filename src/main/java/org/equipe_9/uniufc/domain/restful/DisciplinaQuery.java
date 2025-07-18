b4e997297bbc24a69168c98d36c71dc9556cd924
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-18 11:58:45 -0300

    feat: Add DisciplinaQuery controller for fetching enrolled students and prerequisite disciplines

---
+++ src/main/java/org/equipe_9/uniufc/domain/restful/DisciplinaQuery.java
@@ -0,0 +1,37 @@
+package org.equipe_9.uniufc.domain.restful;
+
+import org.equipe_9.uniufc.domain.service.DisciplinaQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/uniufc")
+public class DisciplinaQuery {
+    private DisciplinaQueryService disciplinaQueryService;
+
+
+    @GetMapping(value = "/alunos-matriculados", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getAlunosDisciplina(@RequestParam("dis") String disciplina) {
+        return ResponseEntity.ok(disciplinaQueryService.findAlunosMatriculadosByDisciplina(disciplina));
+    }
+
+    @GetMapping(value = "/pre-requisitos", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getDisciplinasPR(@RequestParam("dis") String disciplina) {
+        return ResponseEntity.ok(disciplinaQueryService.findDisciplinasPRByDisciplina(disciplina));
+    }
+
+    @GetMapping(value = "/open-disciplinas", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getDisciplinasOpen(@RequestParam("dis") String disciplina) {
+        return ResponseEntity.ok(disciplinaQueryService.findDisciplinasOpenByDisciplina(disciplina));
+    }
+
+    @Autowired
+    private void setDisciplinaQueryService(DisciplinaQueryService disciplinaQueryService) {
+        this.disciplinaQueryService = disciplinaQueryService;
+    }
+}
