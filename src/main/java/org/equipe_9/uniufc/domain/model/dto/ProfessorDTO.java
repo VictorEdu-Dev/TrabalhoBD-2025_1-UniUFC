85b59a881cb2eb7cb4e89df6ca1d80b1ba6621e7
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-19 14:58:56 -0300

    feat: Enhance ProfessorDTO with nested records for detailed professor and discipline information

--- src/main/java/org/equipe_9/uniufc/domain/model/dto/ProfessorDTO.java
+++ src/main/java/org/equipe_9/uniufc/domain/model/dto/ProfessorDTO.java
@@ -1,8 +1,13 @@
 package org.equipe_9.uniufc.domain.model.dto;
 
+import lombok.Builder;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
+@Builder(toBuilder = true)
 public record ProfessorDTO(
         Long id,
         String siape,
@@ -14,7 +19,28 @@ public record ProfessorDTO(
         DepartamentoDTO departamento,
         UsuarioDTO usuario
 ) {
+    @Builder(toBuilder = true)
     public record ProfessoresInfoDTO(
             String nome
     ) {}
+
+    @Builder(toBuilder = true)
+    public record PDInfoDTO(
+            ProfessoresDisciplinasInfoDTO info,
+            List<ProfessoresDisciplinasInfoDTO.ProfessoresDisciplinasDTO> disciplinas
+    ) {
+
+        @Builder(toBuilder = true)
+        public record ProfessoresDisciplinasInfoDTO(
+                String nomeProfessor,
+                BigDecimal somaCreditos
+        ) {
+            @Builder(toBuilder = true)
+            public record ProfessoresDisciplinasDTO(
+                    String nomeDisciplina,
+                    String codigoDisciplina
+            ) {
+            }
+        }
+    }
 }
