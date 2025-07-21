6e4ee1844ba30a1472889fa6469d2a223b2a516f
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-21 18:25:11 -0300

    feat: Update UsuarioDAO to use custom query for finding user by username

--- src/main/java/org/equipe_9/uniufc/domain/repository/UsuarioDAO.java
+++ src/main/java/org/equipe_9/uniufc/domain/repository/UsuarioDAO.java
@@ -1,10 +1,13 @@
 package org.equipe_9.uniufc.domain.repository;
 
 import org.equipe_9.uniufc.domain.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UsuarioDAO extends CrudRepository<Usuario, Long> {
-    Optional<Usuario> findByLogin(String username);
+    @Query("SELECT u FROM usuario u WHERE u.login = :username")
+    Optional<Usuario> findByUsername(@Param("username") String username);
 }
