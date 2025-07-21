24a934fd957b2d62af77a3c0ab539b20b0c61242
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-21 18:24:37 -0300

    feat: Remove unused GetMapping annotations from UniUFCQuery controller

--- src/main/java/org/equipe_9/uniufc/domain/restful/UniUFCQuery.java
+++ src/main/java/org/equipe_9/uniufc/domain/restful/UniUFCQuery.java
@@ -4,7 +4,6 @@ import org.equipe_9.uniufc.domain.service.AlunoQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +13,10 @@ public class UniUFCQuery {
     private AlunoQueryService alunoQueryService;
 
 
-    @GetMapping("/alunos-graduacao")
     public ResponseEntity<?> getAlunosGraduacao() {
         return ResponseEntity.ok(alunoQueryService.getAlunoGraduacao());
     }
 
-    @GetMapping("/disciplinas")
     public ResponseEntity<?> getDisciplinas() {
         return ResponseEntity.ok(alunoQueryService.getDisciplinas());
     }
