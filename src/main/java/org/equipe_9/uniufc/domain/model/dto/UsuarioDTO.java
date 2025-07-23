1a1d7b886611b33c647e63b4ed635ca993070271
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-23 00:07:30 -0300

    feat: add UsuarioRegisterDTO record for user registration details

--- src/main/java/org/equipe_9/uniufc/domain/model/dto/UsuarioDTO.java
+++ src/main/java/org/equipe_9/uniufc/domain/model/dto/UsuarioDTO.java
@@ -1,7 +1,9 @@
 package org.equipe_9.uniufc.domain.model.dto;
 
+import lombok.Builder;
 import org.equipe_9.uniufc.domain.model.Usuario.Regra;
 
+@Builder(toBuilder = true)
 public record UsuarioDTO(
         Long id,
         String nome,
@@ -9,4 +11,10 @@ public record UsuarioDTO(
         String senha,
         Regra regra
 ) {
+    @Builder(toBuilder = true)
+    public record UsuarioRegisterDTO(
+            String nome,
+            String login,
+            String senha
+    ) {}
 }
