1fd287a9e028309af92113d941d487c67acea19f
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-23 00:07:04 -0300

    fix: remove unused User import from AuthCmdService class

--- src/main/java/org/equipe_9/uniufc/domain/service/AuthCmdService.java
+++ src/main/java/org/equipe_9/uniufc/domain/service/AuthCmdService.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
