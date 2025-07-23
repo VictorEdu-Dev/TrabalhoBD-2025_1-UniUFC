82694f9df04c23f64bd0ebecd69e6c5cb889adea
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-23 00:06:50 -0300

    feat: add AlunoRegisterDTO record with builder support to AlunoDTO

--- src/main/java/org/equipe_9/uniufc/domain/model/dto/AlunoDTO.java
+++ src/main/java/org/equipe_9/uniufc/domain/model/dto/AlunoDTO.java
@@ -1,7 +1,12 @@
 package org.equipe_9.uniufc.domain.model.dto;
 
-import java.util.List;
+import lombok.Builder;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Builder(toBuilder = true)
 public record AlunoDTO(
         Long id,
         String matricula,
@@ -12,4 +17,16 @@ public record AlunoDTO(
         List<AlunoHasDisciplinaDTO> disciplinas,
         List<AlunoGraduacaoDTO.TelefoneAlunoDTO> telefones
 ) {
+    @Builder(toBuilder = true)
+    public record AlunoRegisterDTO(
+            UsuarioDTO.UsuarioRegisterDTO usuario,
+            String matricula,
+            String nome,
+            String endereco,
+            String cursoNomeCodigo,
+            LocalDate dataIngresso,
+            List<String> disciplinas,
+            Set<AlunoGraduacaoDTO.TelefoneAlunoDTO> telefones
+    ) {
+    }
 }
